3cfa486ab129605792120de368042b6b790a989d
Sorawit Suriyakarn <thepsint@gmail.com>
2022-02-11 16:09:08 +0700

    Some dependencies and FileIO template

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,4 +1,33 @@
-pub trait IO<'a> {
+use crate::types::Env;
+use serde::Deserialize;
+use std::path::Path;
+
+pub trait IO {
     /// Returns the EVM code to process.
-    fn get_code(&self) -> &'a [u8];
+    fn get_code(&self) -> Vec<u8>;
+
+    /// Returns the next environment to execute in the VM.
+    fn get_next_env(&mut self) -> Option<Env>;
+}
+
+pub struct FileIO {}
+
+impl FileIO {
+    pub fn new(path: &Path) -> Self {
+        let file = std::fs::File::open(path).unwrap();
+        let reader = std::io::BufReader::new(file);
+        // let data: FileIOData = serde_json::from_reader(reader).unwrap();
+        // TODO
+        Self {}
+    }
+}
+
+impl IO for FileIO {
+    fn get_code(&self) -> Vec<u8> {
+        unimplemented!();
+    }
+
+    fn get_next_env(&mut self) -> Option<Env> {
+        unimplemented!();
+    }
 }
